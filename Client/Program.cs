1e7ef5d18c990a37ef95f28ad455bfc4c9867982
KirillNaRabote <kirillznaet@mail.ru>
2024-05-21 03:00:19 +0300

    добавил чат

---
+++ Client/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+class Program
+{
+    public static void StartClient(string host, int port, string message)
+    {
+        try
+        {
+            // Разрешение сетевых имён
+            IPAddress ipAddress = Dns.GetHostAddresses(host)[1];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+
+            // CREATE
+            Socket sender = new Socket(
+                ipAddress.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            try
+            {
+                // CONNECT
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Удалённый адрес подключения сокета: {0}",
+                    sender.RemoteEndPoint!.ToString());
+
+                // Подготовка данных к отправке
+                byte[] msg = Encoding.UTF8.GetBytes($"{message}<EOF>");
+
+                // SEND
+                int bytesSent = sender.Send(msg);
+
+                // RECEIVE
+                byte[] buf = new byte[1024];
+                int bytesRec = sender.Receive(buf);
+                var response = Encoding.UTF8.GetString(buf, 0, bytesRec);
+                response = response.Replace("<EOF>", "");
+
+                Console.WriteLine("Ответ:\n{0}", response);
+
+                // RELEASE
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Invalid parameters");
+            Console.WriteLine("Usage: dotnet run <host> <port> <message>");
+            return;
+        }
+        
+        string host = args[0];
+        int port = int.Parse(args[1]);
+        string message = args[2];
+        
+        StartClient(host, port, message);
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+class Program
+{
+    private static readonly List<string> MessageHistory = new List<string>();
+    
+    public static void StartListening(int port)
+    {
+        // Разрешение сетевых имён
+
+        // Привязываем сокет ко всем интерфейсам на текущей машинe
+        IPAddress ipAddress = IPAddress.Any;
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+        // CREATE
+        Socket listener = new Socket(
+            ipAddress.AddressFamily,
+            SocketType.Stream,
+            ProtocolType.Tcp);
+
+        try
+        {
+            // BIND
+            listener.Bind(localEndPoint);
+
+            // LISTEN
+            listener.Listen(10);
+
+            while (true)
+            {
+                Console.WriteLine("Ожидание соединения клиента...");
+                // ACCEPT
+                Socket handler = listener.Accept();
+
+                Console.WriteLine("Получение данных...");
+                byte[] buf = new byte[1024];
+                string data = null;
+                while (true)
+                {
+                    // RECEIVE
+                    int bytesRec = handler.Receive(buf);
+
+                    data += Encoding.UTF8.GetString(buf, 0, bytesRec);
+                    if (data.IndexOf("<EOF>") > -1)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Полученный текст: {0}", data);
+
+                // Отправляем текст обратно клиенту
+                MessageHistory.Add(data.Replace("<EOF>", ""));
+                string response = "";
+                MessageHistory.ForEach(message => response += $"{message}\n");
+                response += "<EOF>";
+                byte[] msg = Encoding.UTF8.GetBytes(response);
+
+                // SEND
+                handler.Send(msg);
+
+                // RELEASE
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        if (args.Length != 1 || !int.TryParse(args[0], out int port))
+        {
+            Console.WriteLine("Invalid parameters");
+            Console.WriteLine("Usage: dotnet run <port>");
+            return;
+        }
+        
+        Console.WriteLine("Запуск сервера...");
+        StartListening(port);
+
+        Console.WriteLine("\nНажмите ENTER чтобы выйти...");
+        Console.Read();
+    }
+}
